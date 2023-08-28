29bc9d0babd01c6482f3b99bef0c07fd480eaf9e
Kimohc <chomikjakub@gmail.com>
2023-08-28 11:48:02 +0200

    123

--- JS/Home.js
+++ JS/Home.js
@@ -1,6 +1,13 @@
 
 
-function Start()
-{
-    window.location.href="/HTML/Spel.html";
+function Start() {
+    var e = document.getElementById("Category").value;
+    alert(e);
+    window.location.href = "/HTML/Spel.html";
 }
+
+
+
+
+
+

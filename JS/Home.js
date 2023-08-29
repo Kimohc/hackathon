0b89aff6cb019d07ca42664651fa11919dbf26a8
Kimohc <chomikjakub@gmail.com>
2023-08-29 09:15:58 +0200

    3

--- JS/Home.js
+++ JS/Home.js
@@ -3,7 +3,19 @@
 function Start() {
     var e = document.getElementById("Category").value;
     alert(e);
-    window.location.href = "/HTML/Spel.html";
+
+    if(e == "Politiek")
+    {
+    window.location.href = "/HTML/SpelPolitiek.html";
+    }
+    if(e == "Sport")
+    {
+        window.location.href = "/HTML/SpelSport.html"; 
+    }
+    if(e == "Overig")
+    {
+        window.location.href = "/HTML/SpelOverig.html"; 
+    }
 }
 
 
